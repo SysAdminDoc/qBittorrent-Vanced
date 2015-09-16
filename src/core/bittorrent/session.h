ppolewicz/qBittorrent

Call setListeningPort() when state of network interface changes (closes qbittorrent/qBittorrent#475, closes qbittorrent/qBittorrent#3072)

src/core/bittorrent/session.h
@@ -36,6 +36,7 @@
 #include <QVector>
 #include <QMutex>
 #include <QWaitCondition>
+#include <QNetworkConfigurationManager>
 
 #include "core/tristatebool.h"
 #include "core/types.h"
@@ -224,6 +225,10 @@ namespace BitTorrent
         void handleDownloadFailed(const QString &url, const QString &reason);
         void handleRedirectedToMagnet(const QString &url, const QString &magnetUri);
 
+        // Session reconfiguration triggers
+        void onlineStateChanged(const bool online);
+        void networkConfigurationChange(const QNetworkConfiguration&);
+
     private:
         explicit Session(QObject *parent = 0);
         ~Session();
@@ -239,7 +244,7 @@ namespace BitTorrent
         void setProxySettings(libtorrent::proxy_settings proxySettings);
         void adjustLimits();
         void adjustLimits(libtorrent::session_settings &sessionSettings);
-        void setListeningPort(int port);
+        void setListeningPort();
         void setDefaultSavePath(const QString &path);
         void setDefaultTempPath(const QString &path = QString());
         void preAllocateAllFiles(bool b);
@@ -357,6 +362,8 @@ namespace BitTorrent
         QMutex m_alertsMutex;
         QWaitCondition m_alertsWaitCondition;
         QVector<libtorrent::alert *> m_alerts;
+
+        QNetworkConfigurationManager network_manager;
 
         static Session *m_instance;
     };

SysAdminDoc/qBittorrent-Vanced

src/core/bittorrent/session.h
@@ -36,6 +36,7 @@
 #include <QVector>
 #include <QMutex>
 #include <QWaitCondition>
+#include <QNetworkConfigurationManager>
 
 #include "core/tristatebool.h"
 #include "core/types.h"
@@ -227,6 +228,10 @@ namespace BitTorrent
         void handleRedirectedToMagnet(const QString &url, const QString &magnetUri);
         void switchToAlternativeMode(bool alternative);
 
+        // Session reconfiguration triggers
+        void networkOnlineStateChanged(const bool online);
+        void networkConfigurationChange(const QNetworkConfiguration&);
+
     private:
         explicit Session(QObject *parent = 0);
         ~Session();
@@ -242,7 +247,7 @@ namespace BitTorrent
         void setProxySettings(libtorrent::proxy_settings proxySettings);
         void adjustLimits();
         void adjustLimits(libtorrent::session_settings &sessionSettings);
-        void setListeningPort(int port);
+        void setListeningPort();
         void setDefaultSavePath(const QString &path);
         void setDefaultTempPath(const QString &path = QString());
         void preAllocateAllFiles(bool b);
@@ -355,6 +360,8 @@ namespace BitTorrent
         QMutex m_alertsMutex;
         QWaitCondition m_alertsWaitCondition;
         QVector<libtorrent::alert *> m_alerts;
+
+        QNetworkConfigurationManager m_networkManager;
 
         static Session *m_instance;
     };
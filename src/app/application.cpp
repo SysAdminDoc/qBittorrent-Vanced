qbittorrent/qBittorrent

Use SettingsStorage instead.

src/app/application.cpp
@@ -60,8 +60,8 @@
 #include "application.h"
 #include "filelogger.h"
 #include "base/logger.h"
-#include "base/settingsstorage.h"
 #include "base/preferences.h"
+#include "base/settingsstorage.h"
 #include "base/utils/fs.h"
 #include "base/utils/misc.h"
 #include "base/iconprovider.h"
@@ -72,7 +72,23 @@
 #include "base/bittorrent/session.h"
 #include "base/bittorrent/torrenthandle.h"
 
-static const char PARAMS_SEPARATOR[] = "|";
+namespace
+{
+#define FILELOGGER_SETTINGS_KEY(name) "FileLogger/" name
+    const QString KEY_FILELOGGER_ENABLED = FILELOGGER_SETTINGS_KEY("Enabled");
+    const QString KEY_FILELOGGER_PATH = FILELOGGER_SETTINGS_KEY("Path");
+    const QString KEY_FILELOGGER_BACKUP = FILELOGGER_SETTINGS_KEY("Backup");
+    const QString KEY_FILELOGGER_DELETEOLD = FILELOGGER_SETTINGS_KEY("DeleteOld");
+    const QString KEY_FILELOGGER_MAXSIZE = FILELOGGER_SETTINGS_KEY("MaxSize");
+    const QString KEY_FILELOGGER_AGE = FILELOGGER_SETTINGS_KEY("Age");
+    const QString KEY_FILELOGGER_AGETYPE = FILELOGGER_SETTINGS_KEY("AgeType");
+
+    //just a shortcut
+    inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
+
+    const QString LOG_FOLDER("logs");
+    const char PARAMS_SEPARATOR[] = "|";
+}
 
 Application::Application(const QString &id, int &argc, char **argv)
     : BaseApplication(id, argc, argv)
@@ -106,18 +122,104 @@ Application::Application(const QString &id, int &argc, char **argv)
 
     connect(this, SIGNAL(messageReceived(const QString &)), SLOT(processMessage(const QString &)));
     connect(this, SIGNAL(aboutToQuit()), SLOT(cleanup()));
-    connect(Preferences::instance(), SIGNAL(changed()), SLOT(configure()));
 
-    configure();
+    if (isFileLoggerEnabled())
+        m_fileLogger = new FileLogger(getFileLoggerPath(), isFileLoggerBackup(), getFileLoggerMaxSize(), isFileLoggerDeleteOld(), getFileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(getFileLoggerAgeType()));
+
     Logger::instance()->addMessage(tr("qBittorrent %1 started", "qBittorrent v3.2.0alpha started").arg(VERSION));
 }
 
-void Application::configure()
+bool Application::isFileLoggerEnabled() const
 {
-    if (Preferences::instance()->fileLogEnabled() && !m_fileLogger)
-        m_fileLogger = new FileLogger;
+    return settings()->loadValue(KEY_FILELOGGER_ENABLED, true).toBool();
+}
+
+void Application::setFileLoggerEnabled(bool value)
+{
+    if (value && !m_fileLogger)
+        m_fileLogger = new FileLogger(getFileLoggerPath(), isFileLoggerBackup(), getFileLoggerMaxSize(), isFileLoggerDeleteOld(), getFileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(getFileLoggerAgeType()));
     else
         delete m_fileLogger;
+    settings()->storeValue(KEY_FILELOGGER_ENABLED, value);
+}
+
+QString Application::getFileLoggerPath() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_PATH, QVariant(Utils::Fs::QDesktopServicesDataLocation() + LOG_FOLDER)).toString();
+}
+
+void Application::setFileLoggerPath(const QString &value)
+{
+    if (m_fileLogger)
+        m_fileLogger->changePath(value);
+    settings()->storeValue(KEY_FILELOGGER_PATH, value);
+}
+
+bool Application::isFileLoggerBackup() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_BACKUP, true).toBool();
+}
+
+void Application::setFileLoggerBackup(bool value)
+{
+    if (m_fileLogger)
+        m_fileLogger->setBackup(value);
+    settings()->storeValue(KEY_FILELOGGER_BACKUP, value);
+}
+
+bool Application::isFileLoggerDeleteOld() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_DELETEOLD, true).toBool();
+}
+
+void Application::setFileLoggerDeleteOld(bool value)
+{
+    if (value && m_fileLogger)
+        m_fileLogger->deleteOld(getFileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(getFileLoggerAgeType()));
+    settings()->storeValue(KEY_FILELOGGER_DELETEOLD, value);
+}
+
+int Application::getFileLoggerMaxSize() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_MAXSIZE, 10).toInt();
+    if (val < 1)
+        return 1;
+    if (val > 1000)
+        return 1000;
+    return val;
+}
+
+void Application::setFileLoggerMaxSize(const int value)
+{
+    if (m_fileLogger)
+        m_fileLogger->setMaxSize(value);
+    settings()->storeValue(KEY_FILELOGGER_MAXSIZE, std::min(std::max(value, 1), 1000));
+}
+
+int Application::getFileLoggerAge() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_AGE, 6).toInt();
+    if (val < 1)
+        return 1;
+    if (val > 365)
+        return 365;
+    return val;
+}
+
+void Application::setFileLoggerAge(const int value)
+{
+    settings()->storeValue(KEY_FILELOGGER_AGE, std::min(std::max(value, 1), 365));
+}
+
+int Application::getFileLoggerAgeType() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_AGETYPE, 1).toInt();
+    return (val < 0 || val > 2) ? 1 : val;
+}
+
+void Application::setFileLoggerAgeType(const int value)
+{
+    settings()->storeValue(KEY_FILELOGGER_AGETYPE, (value < 0 || value > 2) ? 1 : value);
 }
 
 void Application::processMessage(const QString &message)

src/gui/mainwindow.cpp
@@ -65,6 +65,7 @@
 #include "options_imp.h"
 #include "speedlimitdlg.h"
 #include "base/preferences.h"
+#include "base/settingsstorage.h"
 #include "trackerlist.h"
 #include "peerlistwidget.h"
 #include "transferlistfilterswidget.h"
@@ -93,6 +94,16 @@ void qt_mac_set_dock_menu(QMenu *menu);
 
 #define TIME_TRAY_BALLOON 5000
 #define PREVENT_SUSPEND_INTERVAL 60000
+
+namespace
+{
+#define EXECUTIONLOG_SETTINGS_KEY(name) "ExecutionLog/" name
+    const QString KEY_EXECUTIONLOG_ENABLED = EXECUTIONLOG_SETTINGS_KEY("Enabled");
+    const QString KEY_EXECUTIONLOG_TYPES = EXECUTIONLOG_SETTINGS_KEY("Types");
+
+    //just a shortcut
+    inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
+}
 
 /*****************************************************
 *                                                   *
@@ -273,9 +284,9 @@ MainWindow::MainWindow(QWidget *parent)
     actionSpeed_in_title_bar->setChecked(pref->speedInTitleBar());
     actionRSS_Reader->setChecked(pref->isRSSEnabled());
     actionSearch_engine->setChecked(pref->isSearchEnabled());
-    actionExecutionLogs->setChecked(pref->isExecutionLogEnabled());
+    actionExecutionLogs->setChecked(isExecutionLogEnabled());
 
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(getExecutionLogMsgTypes());
     actionNormalMessages->setChecked(flags & Log::NORMAL);
     actionInformationMessages->setChecked(flags & Log::INFO);
     actionWarningMessages->setChecked(flags & Log::WARNING);
@@ -380,6 +391,29 @@ MainWindow::~MainWindow()
     // Workaround to avoid bug http://bugreports.qt.nokia.com/browse/QTBUG-7305
     setUnifiedTitleAndToolBarOnMac(false);
 #endif
+}
+
+bool MainWindow::isExecutionLogEnabled() const
+{
+    return settings()->loadValue(KEY_EXECUTIONLOG_ENABLED, false).toBool();
+}
+
+void MainWindow::setExecutionLogEnabled(bool value)
+{
+    settings()->storeValue(KEY_EXECUTIONLOG_ENABLED, value);
+}
+
+int MainWindow::getExecutionLogMsgTypes() const
+{
+    // as default value we need all the bits set
+    // -1 is considered the portable way to achieve that
+    return settings()->loadValue(KEY_EXECUTIONLOG_TYPES, -1).toInt();
+}
+
+void MainWindow::setExecutionLogMsgTypes(const int value)
+{
+    m_executionLog->showMsgTypes(static_cast<Log::MsgTypes>(value));
+    settings()->storeValue(KEY_EXECUTIONLOG_TYPES, value);
 }
 
 void MainWindow::addToolbarContextMenu()
@@ -1522,7 +1556,7 @@ void MainWindow::on_actionExecutionLogs_triggered(bool checked)
 {
     if (checked) {
         Q_ASSERT(!m_executionLog);
-        m_executionLog = new ExecutionLog(tabs);
+        m_executionLog = new ExecutionLog(tabs, static_cast<Log::MsgType>(getExecutionLogMsgTypes()));
         int index_tab = tabs->addTab(m_executionLog, tr("Execution Log"));
         tabs->setTabIcon(index_tab, GuiIconProvider::instance()->getIcon("view-calendar-journal"));
     }
@@ -1534,7 +1568,7 @@ void MainWindow::on_actionExecutionLogs_triggered(bool checked)
     actionInformationMessages->setEnabled(checked);
     actionWarningMessages->setEnabled(checked);
     actionCriticalMessages->setEnabled(checked);
-    Preferences::instance()->setExecutionLogEnabled(checked);
+    setExecutionLogEnabled(checked);
 }
 
 void MainWindow::on_actionNormalMessages_triggered(bool checked)
@@ -1542,12 +1576,9 @@ void MainWindow::on_actionNormalMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(getExecutionLogMsgTypes());
     checked ? (flags |= Log::NORMAL) : (flags &= ~Log::NORMAL);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionInformationMessages_triggered(bool checked)
@@ -1555,12 +1586,9 @@ void MainWindow::on_actionInformationMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(getExecutionLogMsgTypes());
     checked ? (flags |= Log::INFO) : (flags &= ~Log::INFO);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionWarningMessages_triggered(bool checked)
@@ -1568,12 +1596,9 @@ void MainWindow::on_actionWarningMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(getExecutionLogMsgTypes());
     checked ? (flags |= Log::WARNING) : (flags &= ~Log::WARNING);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionCriticalMessages_triggered(bool checked)
@@ -1581,12 +1606,9 @@ void MainWindow::on_actionCriticalMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(getExecutionLogMsgTypes());
     checked ? (flags |= Log::CRITICAL) : (flags &= ~Log::CRITICAL);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionAutoExit_qBittorrent_toggled(bool enabled)

SysAdminDoc/qBittorrent-Vanced

src/app/application.cpp
@@ -60,8 +60,8 @@
 #include "application.h"
 #include "filelogger.h"
 #include "base/logger.h"
-#include "base/settingsstorage.h"
 #include "base/preferences.h"
+#include "base/settingsstorage.h"
 #include "base/utils/fs.h"
 #include "base/utils/misc.h"
 #include "base/iconprovider.h"
@@ -72,7 +72,26 @@
 #include "base/bittorrent/session.h"
 #include "base/bittorrent/torrenthandle.h"
 
-static const char PARAMS_SEPARATOR[] = "|";
+namespace
+{
+#define SETTINGS_KEY(name) "Application/" name
+
+    // FileLogger properties keys
+#define FILELOGGER_SETTINGS_KEY(name) SETTINGS_KEY("FileLogger/") name
+    const QString KEY_FILELOGGER_ENABLED = FILELOGGER_SETTINGS_KEY("Enabled");
+    const QString KEY_FILELOGGER_PATH = FILELOGGER_SETTINGS_KEY("Path");
+    const QString KEY_FILELOGGER_BACKUP = FILELOGGER_SETTINGS_KEY("Backup");
+    const QString KEY_FILELOGGER_DELETEOLD = FILELOGGER_SETTINGS_KEY("DeleteOld");
+    const QString KEY_FILELOGGER_MAXSIZE = FILELOGGER_SETTINGS_KEY("MaxSize");
+    const QString KEY_FILELOGGER_AGE = FILELOGGER_SETTINGS_KEY("Age");
+    const QString KEY_FILELOGGER_AGETYPE = FILELOGGER_SETTINGS_KEY("AgeType");
+
+    //just a shortcut
+    inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
+
+    const QString LOG_FOLDER("logs");
+    const char PARAMS_SEPARATOR[] = "|";
+}
 
 Application::Application(const QString &id, int &argc, char **argv)
     : BaseApplication(id, argc, argv)
@@ -106,20 +125,104 @@ Application::Application(const QString &id, int &argc, char **argv)
 
     connect(this, SIGNAL(messageReceived(const QString &)), SLOT(processMessage(const QString &)));
     connect(this, SIGNAL(aboutToQuit()), SLOT(cleanup()));
-    connect(Preferences::instance(), SIGNAL(changed()), SLOT(configure()));
 
-    configure();
+    if (isFileLoggerEnabled())
+        m_fileLogger = new FileLogger(fileLoggerPath(), isFileLoggerBackup(), fileLoggerMaxSize(), isFileLoggerDeleteOld(), fileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(fileLoggerAgeType()));
+
     Logger::instance()->addMessage(tr("qBittorrent %1 started", "qBittorrent v3.2.0alpha started").arg(VERSION));
 }
 
-void Application::configure()
+bool Application::isFileLoggerEnabled() const
 {
-    bool fileLogEnabled = Preferences::instance()->fileLogEnabled();
+    return settings()->loadValue(KEY_FILELOGGER_ENABLED, true).toBool();
+}
 
-    if (fileLogEnabled && !m_fileLogger)
-        m_fileLogger = new FileLogger;
-    else if (!fileLogEnabled)
+void Application::setFileLoggerEnabled(bool value)
+{
+    if (value && !m_fileLogger)
+        m_fileLogger = new FileLogger(fileLoggerPath(), isFileLoggerBackup(), fileLoggerMaxSize(), isFileLoggerDeleteOld(), fileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(fileLoggerAgeType()));
+    else if (!value)
         delete m_fileLogger;
+    settings()->storeValue(KEY_FILELOGGER_ENABLED, value);
+}
+
+QString Application::fileLoggerPath() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_PATH, QVariant(Utils::Fs::QDesktopServicesDataLocation() + LOG_FOLDER)).toString();
+}
+
+void Application::setFileLoggerPath(const QString &value)
+{
+    if (m_fileLogger)
+        m_fileLogger->changePath(value);
+    settings()->storeValue(KEY_FILELOGGER_PATH, value);
+}
+
+bool Application::isFileLoggerBackup() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_BACKUP, true).toBool();
+}
+
+void Application::setFileLoggerBackup(bool value)
+{
+    if (m_fileLogger)
+        m_fileLogger->setBackup(value);
+    settings()->storeValue(KEY_FILELOGGER_BACKUP, value);
+}
+
+bool Application::isFileLoggerDeleteOld() const
+{
+    return settings()->loadValue(KEY_FILELOGGER_DELETEOLD, true).toBool();
+}
+
+void Application::setFileLoggerDeleteOld(bool value)
+{
+    if (value && m_fileLogger)
+        m_fileLogger->deleteOld(fileLoggerAge(), static_cast<FileLogger::FileLogAgeType>(fileLoggerAgeType()));
+    settings()->storeValue(KEY_FILELOGGER_DELETEOLD, value);
+}
+
+int Application::fileLoggerMaxSize() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_MAXSIZE, 10).toInt();
+    if (val < 1)
+        return 1;
+    if (val > 1000)
+        return 1000;
+    return val;
+}
+
+void Application::setFileLoggerMaxSize(const int value)
+{
+    if (m_fileLogger)
+        m_fileLogger->setMaxSize(value);
+    settings()->storeValue(KEY_FILELOGGER_MAXSIZE, std::min(std::max(value, 1), 1000));
+}
+
+int Application::fileLoggerAge() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_AGE, 6).toInt();
+    if (val < 1)
+        return 1;
+    if (val > 365)
+        return 365;
+    return val;
+}
+
+void Application::setFileLoggerAge(const int value)
+{
+    settings()->storeValue(KEY_FILELOGGER_AGE, std::min(std::max(value, 1), 365));
+}
+
+int Application::fileLoggerAgeType() const
+{
+    int val = settings()->loadValue(KEY_FILELOGGER_AGETYPE, 1).toInt();
+    return (val < 0 || val > 2) ? 1 : val;
+}
+
+void Application::setFileLoggerAgeType(const int value)
+{
+    settings()->storeValue(KEY_FILELOGGER_AGETYPE, (value < 0 || value > 2) ? 1 : value);
 }
 
 void Application::processMessage(const QString &message)

src/gui/mainwindow.cpp
@@ -65,6 +65,7 @@
 #include "options_imp.h"
 #include "speedlimitdlg.h"
 #include "base/preferences.h"
+#include "base/settingsstorage.h"
 #include "trackerlist.h"
 #include "peerlistwidget.h"
 #include "transferlistfilterswidget.h"
@@ -93,6 +94,19 @@ void qt_mac_set_dock_menu(QMenu *menu);
 
 #define TIME_TRAY_BALLOON 5000
 #define PREVENT_SUSPEND_INTERVAL 60000
+
+namespace
+{
+#define SETTINGS_KEY(name) "MainWindow/" name
+
+    // ExecutionLog properties keys
+#define EXECUTIONLOG_SETTINGS_KEY(name) SETTINGS_KEY("ExecutionLog/") name
+    const QString KEY_EXECUTIONLOG_ENABLED = EXECUTIONLOG_SETTINGS_KEY("Enabled");
+    const QString KEY_EXECUTIONLOG_TYPES = EXECUTIONLOG_SETTINGS_KEY("Types");
+
+    //just a shortcut
+    inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
+}
 
 /*****************************************************
 *                                                   *
@@ -273,9 +287,9 @@ MainWindow::MainWindow(QWidget *parent)
     actionSpeed_in_title_bar->setChecked(pref->speedInTitleBar());
     actionRSS_Reader->setChecked(pref->isRSSEnabled());
     actionSearch_engine->setChecked(pref->isSearchEnabled());
-    actionExecutionLogs->setChecked(pref->isExecutionLogEnabled());
+    actionExecutionLogs->setChecked(isExecutionLogEnabled());
 
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(executionLogMsgTypes());
     actionNormalMessages->setChecked(flags & Log::NORMAL);
     actionInformationMessages->setChecked(flags & Log::INFO);
     actionWarningMessages->setChecked(flags & Log::WARNING);
@@ -380,6 +394,29 @@ MainWindow::~MainWindow()
     // Workaround to avoid bug http://bugreports.qt.nokia.com/browse/QTBUG-7305
     setUnifiedTitleAndToolBarOnMac(false);
 #endif
+}
+
+bool MainWindow::isExecutionLogEnabled() const
+{
+    return settings()->loadValue(KEY_EXECUTIONLOG_ENABLED, false).toBool();
+}
+
+void MainWindow::setExecutionLogEnabled(bool value)
+{
+    settings()->storeValue(KEY_EXECUTIONLOG_ENABLED, value);
+}
+
+int MainWindow::executionLogMsgTypes() const
+{
+    // as default value we need all the bits set
+    // -1 is considered the portable way to achieve that
+    return settings()->loadValue(KEY_EXECUTIONLOG_TYPES, -1).toInt();
+}
+
+void MainWindow::setExecutionLogMsgTypes(const int value)
+{
+    m_executionLog->showMsgTypes(static_cast<Log::MsgTypes>(value));
+    settings()->storeValue(KEY_EXECUTIONLOG_TYPES, value);
 }
 
 void MainWindow::addToolbarContextMenu()
@@ -1522,7 +1559,7 @@ void MainWindow::on_actionExecutionLogs_triggered(bool checked)
 {
     if (checked) {
         Q_ASSERT(!m_executionLog);
-        m_executionLog = new ExecutionLog(tabs);
+        m_executionLog = new ExecutionLog(tabs, static_cast<Log::MsgType>(executionLogMsgTypes()));
         int index_tab = tabs->addTab(m_executionLog, tr("Execution Log"));
         tabs->setTabIcon(index_tab, GuiIconProvider::instance()->getIcon("view-calendar-journal"));
     }
@@ -1534,7 +1571,7 @@ void MainWindow::on_actionExecutionLogs_triggered(bool checked)
     actionInformationMessages->setEnabled(checked);
     actionWarningMessages->setEnabled(checked);
     actionCriticalMessages->setEnabled(checked);
-    Preferences::instance()->setExecutionLogEnabled(checked);
+    setExecutionLogEnabled(checked);
 }
 
 void MainWindow::on_actionNormalMessages_triggered(bool checked)
@@ -1542,12 +1579,9 @@ void MainWindow::on_actionNormalMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(executionLogMsgTypes());
     checked ? (flags |= Log::NORMAL) : (flags &= ~Log::NORMAL);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionInformationMessages_triggered(bool checked)
@@ -1555,12 +1589,9 @@ void MainWindow::on_actionInformationMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(executionLogMsgTypes());
     checked ? (flags |= Log::INFO) : (flags &= ~Log::INFO);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionWarningMessages_triggered(bool checked)
@@ -1568,12 +1599,9 @@ void MainWindow::on_actionWarningMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(executionLogMsgTypes());
     checked ? (flags |= Log::WARNING) : (flags &= ~Log::WARNING);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionCriticalMessages_triggered(bool checked)
@@ -1581,12 +1609,9 @@ void MainWindow::on_actionCriticalMessages_triggered(bool checked)
     if (!m_executionLog)
         return;
 
-    Preferences* const pref = Preferences::instance();
-
-    Log::MsgTypes flags(pref->executionLogMessageTypes());
+    Log::MsgTypes flags(executionLogMsgTypes());
     checked ? (flags |= Log::CRITICAL) : (flags &= ~Log::CRITICAL);
-    m_executionLog->showMsgTypes(flags);
-    pref->setExecutionLogMessageTypes(flags);
+    setExecutionLogMsgTypes(flags);
 }
 
 void MainWindow::on_actionAutoExit_qBittorrent_toggled(bool enabled)
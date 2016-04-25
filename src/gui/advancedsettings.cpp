qbittorrent/qBittorrent

Display notifications when a torrent is added. Closes #334 and #915.

src/gui/advancedsettings.cpp
@@ -31,7 +31,9 @@
 #include <QHeaderView>
 #include <QHostAddress>
 #include <QNetworkInterface>
+#include "app/application.h"
 #include "base/preferences.h"
+#include "gui/mainwindow.h"
 
 enum AdvSettingsCols
 {
@@ -58,6 +60,7 @@ enum AdvSettingsRows
     RESOLVE_HOSTS,
     RESOLVE_COUNTRIES,
     PROGRAM_NOTIFICATIONS,
+    TORRENT_ADDED_NOTIFICATIONS,
 #if (defined(Q_OS_UNIX) && !defined(Q_OS_MAC))
     USE_ICON_THEME,
 #endif
@@ -150,7 +153,9 @@ void AdvancedSettings::saveAdvancedSettings()
     else
         pref->setNetworkAddress(addr.toString());
     // Program notification
-    pref->useProgramNotification(cb_program_notifications.isChecked());
+    MainWindow * const mainWindow = static_cast<Application*>(QCoreApplication::instance())->mainWindow();
+    mainWindow->setNotificationsEnabled(cb_program_notifications.isChecked());
+    mainWindow->setAddedTorrentNotificationsEnabled(cb_added_torrent_notifications.isChecked());
     // Tracker
     pref->setTrackerEnabled(cb_tracker_status.isChecked());
     pref->setTrackerPort(spin_tracker_port.value());
@@ -278,8 +283,12 @@ void AdvancedSettings::loadAdvancedSettings()
     txt_network_address.setText(pref->getNetworkAddress());
     addRow(NETWORK_ADDRESS, tr("IP Address to report to trackers (requires restart)"), &txt_network_address);
     // Program notifications
-    cb_program_notifications.setChecked(pref->useProgramNotification());
-    addRow(PROGRAM_NOTIFICATIONS, tr("Display program on-screen notifications"), &cb_program_notifications);
+    const MainWindow * const mainWindow = static_cast<Application*>(QCoreApplication::instance())->mainWindow();
+    cb_program_notifications.setChecked(mainWindow->isNotificationsEnabled());
+    addRow(PROGRAM_NOTIFICATIONS, tr("Display notifications"), &cb_program_notifications);
+    // Torrent added notifications
+    cb_added_torrent_notifications.setChecked(mainWindow->isAddedTorrentNotificationsEnabled());
+    addRow(TORRENT_ADDED_NOTIFICATIONS, tr("Display notifications for added torrents"), &cb_added_torrent_notifications);
     // Tracker State
     cb_tracker_status.setChecked(pref->isTrackerEnabled());
     addRow(TRACKER_STATUS, tr("Enable embedded tracker"), &cb_tracker_status);

src/gui/advancedsettings.h
@@ -60,7 +60,7 @@ private:
     QLabel labelQbtLink, labelLibtorrentLink;
     QSpinBox spin_cache, spin_save_resume_data_interval, outgoing_ports_min, outgoing_ports_max, spin_list_refresh, spin_maxhalfopen, spin_tracker_port, spin_cache_ttl;
     QCheckBox cb_os_cache, cb_recheck_completed, cb_resolve_countries, cb_resolve_hosts,
-              cb_super_seeding, cb_program_notifications, cb_tracker_status,
+              cb_super_seeding, cb_program_notifications, cb_added_torrent_notifications, cb_tracker_status,
               cb_confirm_torrent_recheck, cb_enable_tracker_ext, cb_listen_ipv6, cb_announce_all_trackers;
     QComboBox combo_iface;
     QLineEdit txt_network_address;

src/gui/mainwindow.cpp
@@ -101,12 +101,17 @@ void qt_mac_set_dock_menu(QMenu *menu);
 
 namespace
 {
-#define SETTINGS_KEY(name) "MainWindow/" name
+#define SETTINGS_KEY(name) "GUI/" name
 
     // ExecutionLog properties keys
-#define EXECUTIONLOG_SETTINGS_KEY(name) SETTINGS_KEY("ExecutionLog/") name
+#define EXECUTIONLOG_SETTINGS_KEY(name) SETTINGS_KEY("Log/") name
     const QString KEY_EXECUTIONLOG_ENABLED = EXECUTIONLOG_SETTINGS_KEY("Enabled");
     const QString KEY_EXECUTIONLOG_TYPES = EXECUTIONLOG_SETTINGS_KEY("Types");
+
+    // Notifications properties keys
+#define NOTIFICATIONS_SETTINGS_KEY(name) SETTINGS_KEY("Notifications/") name
+    const QString KEY_NOTIFICATIONS_ENABLED = NOTIFICATIONS_SETTINGS_KEY("Enabled");
+    const QString KEY_NOTIFICATIONS_ADDEDTORRENT = NOTIFICATIONS_SETTINGS_KEY("AddedTorrent");
 
     //just a shortcut
     inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
@@ -181,6 +186,7 @@ MainWindow::MainWindow(QWidget *parent)
     // Creating Bittorrent session
     connect(BitTorrent::Session::instance(), SIGNAL(fullDiskError(BitTorrent::TorrentHandle *const, QString)), this, SLOT(fullDiskError(BitTorrent::TorrentHandle *const, QString)));
     connect(BitTorrent::Session::instance(), SIGNAL(addTorrentFailed(const QString &)), this, SLOT(addTorrentFailed(const QString &)));
+    connect(BitTorrent::Session::instance(), SIGNAL(torrentAdded(BitTorrent::TorrentHandle *const)), this, SLOT(addedTorrent(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(torrentFinished(BitTorrent::TorrentHandle *const)), this, SLOT(finishedTorrent(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(trackerAuthenticationRequired(BitTorrent::TorrentHandle *const)), this, SLOT(trackerAuthenticationRequired(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(downloadFromUrlFailed(QString, QString)), this, SLOT(handleDownloadFromUrlFailure(QString, QString)));
@@ -419,6 +425,26 @@ void MainWindow::setExecutionLogMsgTypes(const int value)
 {
     m_executionLog->showMsgTypes(static_cast<Log::MsgTypes>(value));
     settings()->storeValue(KEY_EXECUTIONLOG_TYPES, value);
+}
+
+bool MainWindow::isNotificationsEnabled() const
+{
+    return settings()->loadValue(KEY_NOTIFICATIONS_ENABLED, true).toBool();
+}
+
+void MainWindow::setNotificationsEnabled(bool value)
+{
+    settings()->storeValue(KEY_NOTIFICATIONS_ENABLED, value);
+}
+
+bool MainWindow::isAddedTorrentNotificationsEnabled() const
+{
+    return settings()->loadValue(KEY_NOTIFICATIONS_ADDEDTORRENT, false).toBool();
+}
+
+void MainWindow::setAddedTorrentNotificationsEnabled(bool value)
+{
+    settings()->storeValue(KEY_NOTIFICATIONS_ADDEDTORRENT, value);
 }
 
 void MainWindow::addToolbarContextMenu()
@@ -694,6 +720,13 @@ void MainWindow::balloonClicked()
 void MainWindow::addTorrentFailed(const QString &error) const
 {
     showNotificationBaloon(tr("Error"), tr("Failed to add torrent: %1").arg(error));
+}
+
+// called when a torrent was added
+void MainWindow::addedTorrent(BitTorrent::TorrentHandle *const torrent) const
+{
+    if (isAddedTorrentNotificationsEnabled())
+        showNotificationBaloon(tr("Torrent added"), tr("'%1' was added.", "e.g: xxx.avi was added.").arg(torrent->name()));
 }
 
 // called when a torrent has finished
@@ -1279,7 +1312,7 @@ void MainWindow::updateGUI()
 
 void MainWindow::showNotificationBaloon(QString title, QString msg) const
 {
-    if (!Preferences::instance()->useProgramNotification()) return;
+    if (!isNotificationsEnabled()) return;
 #if (defined(Q_OS_UNIX) && !defined(Q_OS_MAC)) && defined(QT_DBUS_LIB)
     org::freedesktop::Notifications notifications("org.freedesktop.Notifications",
                                                   "/org/freedesktop/Notifications",

SysAdminDoc/qBittorrent-Vanced

src/gui/advancedsettings.cpp
@@ -31,7 +31,9 @@
 #include <QHeaderView>
 #include <QHostAddress>
 #include <QNetworkInterface>
+#include "app/application.h"
 #include "base/preferences.h"
+#include "gui/mainwindow.h"
 
 enum AdvSettingsCols
 {
@@ -58,6 +60,7 @@ enum AdvSettingsRows
     RESOLVE_HOSTS,
     RESOLVE_COUNTRIES,
     PROGRAM_NOTIFICATIONS,
+    TORRENT_ADDED_NOTIFICATIONS,
 #if (defined(Q_OS_UNIX) && !defined(Q_OS_MAC))
     USE_ICON_THEME,
 #endif
@@ -150,7 +153,9 @@ void AdvancedSettings::saveAdvancedSettings()
     else
         pref->setNetworkAddress(addr.toString());
     // Program notification
-    pref->useProgramNotification(cb_program_notifications.isChecked());
+    MainWindow * const mainWindow = static_cast<Application*>(QCoreApplication::instance())->mainWindow();
+    mainWindow->setNotificationsEnabled(cb_program_notifications.isChecked());
+    mainWindow->setTorrentAddedNotificationsEnabled(cb_torrent_added_notifications.isChecked());
     // Tracker
     pref->setTrackerEnabled(cb_tracker_status.isChecked());
     pref->setTrackerPort(spin_tracker_port.value());
@@ -284,8 +289,12 @@ void AdvancedSettings::loadAdvancedSettings()
     txt_network_address.setText(pref->getNetworkAddress());
     addRow(NETWORK_ADDRESS, tr("IP Address to report to trackers (requires restart)"), &txt_network_address);
     // Program notifications
-    cb_program_notifications.setChecked(pref->useProgramNotification());
-    addRow(PROGRAM_NOTIFICATIONS, tr("Display program on-screen notifications"), &cb_program_notifications);
+    const MainWindow * const mainWindow = static_cast<Application*>(QCoreApplication::instance())->mainWindow();
+    cb_program_notifications.setChecked(mainWindow->isNotificationsEnabled());
+    addRow(PROGRAM_NOTIFICATIONS, tr("Display notifications"), &cb_program_notifications);
+    // Torrent added notifications
+    cb_torrent_added_notifications.setChecked(mainWindow->isTorrentAddedNotificationsEnabled());
+    addRow(TORRENT_ADDED_NOTIFICATIONS, tr("Display notifications for added torrents"), &cb_torrent_added_notifications);
     // Tracker State
     cb_tracker_status.setChecked(pref->isTrackerEnabled());
     addRow(TRACKER_STATUS, tr("Enable embedded tracker"), &cb_tracker_status);

src/gui/advancedsettings.h
@@ -60,7 +60,7 @@ private:
     QLabel labelQbtLink, labelLibtorrentLink;
     QSpinBox spin_cache, spin_save_resume_data_interval, outgoing_ports_min, outgoing_ports_max, spin_list_refresh, spin_maxhalfopen, spin_tracker_port, spin_cache_ttl;
     QCheckBox cb_os_cache, cb_recheck_completed, cb_resolve_countries, cb_resolve_hosts,
-              cb_super_seeding, cb_program_notifications, cb_tracker_status,
+              cb_super_seeding, cb_program_notifications, cb_torrent_added_notifications, cb_tracker_status,
               cb_confirm_torrent_recheck, cb_enable_tracker_ext, cb_listen_ipv6, cb_announce_all_trackers;
     QComboBox combo_iface;
     QLineEdit txt_network_address;

src/gui/mainwindow.cpp
@@ -101,12 +101,17 @@ void qt_mac_set_dock_menu(QMenu *menu);
 
 namespace
 {
-#define SETTINGS_KEY(name) "MainWindow/" name
+#define SETTINGS_KEY(name) "GUI/" name
 
     // ExecutionLog properties keys
-#define EXECUTIONLOG_SETTINGS_KEY(name) SETTINGS_KEY("ExecutionLog/") name
+#define EXECUTIONLOG_SETTINGS_KEY(name) SETTINGS_KEY("Log/") name
     const QString KEY_EXECUTIONLOG_ENABLED = EXECUTIONLOG_SETTINGS_KEY("Enabled");
     const QString KEY_EXECUTIONLOG_TYPES = EXECUTIONLOG_SETTINGS_KEY("Types");
+
+    // Notifications properties keys
+#define NOTIFICATIONS_SETTINGS_KEY(name) SETTINGS_KEY("Notifications/") name
+    const QString KEY_NOTIFICATIONS_ENABLED = NOTIFICATIONS_SETTINGS_KEY("Enabled");
+    const QString KEY_NOTIFICATIONS_TORRENTADDED = NOTIFICATIONS_SETTINGS_KEY("TorrentAdded");
 
     //just a shortcut
     inline SettingsStorage *settings() { return  SettingsStorage::instance(); }
@@ -181,6 +186,7 @@ MainWindow::MainWindow(QWidget *parent)
     // Creating Bittorrent session
     connect(BitTorrent::Session::instance(), SIGNAL(fullDiskError(BitTorrent::TorrentHandle *const, QString)), this, SLOT(fullDiskError(BitTorrent::TorrentHandle *const, QString)));
     connect(BitTorrent::Session::instance(), SIGNAL(addTorrentFailed(const QString &)), this, SLOT(addTorrentFailed(const QString &)));
+    connect(BitTorrent::Session::instance(), SIGNAL(torrentAdded(BitTorrent::TorrentHandle *const)), this, SLOT(torrentAdded(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(torrentFinished(BitTorrent::TorrentHandle *const)), this, SLOT(finishedTorrent(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(trackerAuthenticationRequired(BitTorrent::TorrentHandle *const)), this, SLOT(trackerAuthenticationRequired(BitTorrent::TorrentHandle *const)));
     connect(BitTorrent::Session::instance(), SIGNAL(downloadFromUrlFailed(QString, QString)), this, SLOT(handleDownloadFromUrlFailure(QString, QString)));
@@ -419,6 +425,26 @@ void MainWindow::setExecutionLogMsgTypes(const int value)
 {
     m_executionLog->showMsgTypes(static_cast<Log::MsgTypes>(value));
     settings()->storeValue(KEY_EXECUTIONLOG_TYPES, value);
+}
+
+bool MainWindow::isNotificationsEnabled() const
+{
+    return settings()->loadValue(KEY_NOTIFICATIONS_ENABLED, true).toBool();
+}
+
+void MainWindow::setNotificationsEnabled(bool value)
+{
+    settings()->storeValue(KEY_NOTIFICATIONS_ENABLED, value);
+}
+
+bool MainWindow::isTorrentAddedNotificationsEnabled() const
+{
+    return settings()->loadValue(KEY_NOTIFICATIONS_TORRENTADDED, false).toBool();
+}
+
+void MainWindow::setTorrentAddedNotificationsEnabled(bool value)
+{
+    settings()->storeValue(KEY_NOTIFICATIONS_TORRENTADDED, value);
 }
 
 void MainWindow::addToolbarContextMenu()
@@ -694,6 +720,13 @@ void MainWindow::balloonClicked()
 void MainWindow::addTorrentFailed(const QString &error) const
 {
     showNotificationBaloon(tr("Error"), tr("Failed to add torrent: %1").arg(error));
+}
+
+// called when a torrent was added
+void MainWindow::torrentAdded(BitTorrent::TorrentHandle *const torrent) const
+{
+    if (isTorrentAddedNotificationsEnabled())
+        showNotificationBaloon(tr("Torrent added"), tr("'%1' was added.", "e.g: xxx.avi was added.").arg(torrent->name()));
 }
 
 // called when a torrent has finished
@@ -1279,7 +1312,7 @@ void MainWindow::updateGUI()
 
 void MainWindow::showNotificationBaloon(QString title, QString msg) const
 {
-    if (!Preferences::instance()->useProgramNotification()) return;
+    if (!isNotificationsEnabled()) return;
 #if (defined(Q_OS_UNIX) && !defined(Q_OS_MAC)) && defined(QT_DBUS_LIB)
     org::freedesktop::Notifications notifications("org.freedesktop.Notifications",
                                                   "/org/freedesktop/Notifications",
SysAdminDoc/qBittorrent-Vanced

commit 82a1c389b7ebe434a189280cec3794b3d188d326

Don't ever stop seeding forced torrents. Closes #5784.

src/base/bittorrent/session.cpp
@@ -1289,7 +1289,8 @@ void Session::processBigRatios()
 
     qreal globalMaxRatio = this->globalMaxRatio();
     foreach (TorrentHandle *const torrent, m_torrents) {
-        if (torrent->isSeed() && (torrent->ratioLimit() != TorrentHandle::NO_RATIO_LIMIT)) {
+        if (torrent->isSeed()
+            && ((torrent->ratioLimit() != TorrentHandle::NO_RATIO_LIMIT) || !torrent->isForced())) {
             const qreal ratio = torrent->realRatio();
             qreal ratioLimit = torrent->ratioLimit();
             if (ratioLimit == TorrentHandle::USE_GLOBAL_RATIO) {

commit bcba311df2e90cbf233a0f594239b2a0b4a18733

Fix share ratio limiting. Broken by commit 82a1c389b7ebe43. Closes #6039 #6048.

src/base/bittorrent/session.cpp
@@ -1292,7 +1292,8 @@ void Session::processBigRatios()
     qreal globalMaxRatio = this->globalMaxRatio();
     foreach (TorrentHandle *const torrent, m_torrents) {
         if (torrent->isSeed()
-            && ((torrent->ratioLimit() != TorrentHandle::NO_RATIO_LIMIT) || !torrent->isForced())) {
+            && (torrent->ratioLimit() != TorrentHandle::NO_RATIO_LIMIT)
+            && !torrent->isForced()) {
             const qreal ratio = torrent->realRatio();
             qreal ratioLimit = torrent->ratioLimit();
             if (ratioLimit == TorrentHandle::USE_GLOBAL_RATIO) {
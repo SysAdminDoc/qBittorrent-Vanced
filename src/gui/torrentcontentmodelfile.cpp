evsh/qBittorrent

Add availability column to torrent content model

src/gui/torrentcontentmodelfile.cpp
@@ -72,3 +72,9 @@ void TorrentContentModelFile::setProgress(qreal progress)
     m_remaining = (qulonglong)(m_size * (1.0 - m_progress));
     Q_ASSERT(m_progress <= 1.);
 }
+
+void TorrentContentModelFile::setAvalability(qreal avalability)
+{
+    m_availability = avalability;
+    Q_ASSERT(m_availability <= 1.);
+}

src/gui/torrentcontentmodelfile.h
@@ -42,6 +42,8 @@ public:
     int fileIndex() const;
     void setPriority(int new_prio, bool update_parent = true);
     void setProgress(qreal progress);
+    void setAvalability(qreal avalability);
+
     ItemType itemType() const { return FileType; }
 
 private:

SysAdminDoc/qBittorrent-Vanced

src/gui/torrentcontentmodelfile.cpp
@@ -73,6 +73,12 @@ void TorrentContentModelFile::setProgress(qreal progress)
     Q_ASSERT(m_progress <= 1.);
 }
 
+void TorrentContentModelFile::setAvailability(qreal availability)
+{
+    m_availability = availability;
+    Q_ASSERT(m_availability <= 1.);
+}
+
 TorrentContentModelItem::ItemType TorrentContentModelFile::itemType() const
 {
     return FileType;

src/gui/torrentcontentmodelfile.h
@@ -42,6 +42,7 @@ public:
     int fileIndex() const;
     void setPriority(int newPriority, bool updateParent = true) override;
     void setProgress(qreal progress);
+    void setAvailability(qreal availability);
     ItemType itemType() const override;
 
 private: